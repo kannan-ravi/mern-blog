3f79e1a8b13867751dfc60b0754f4fb5b10a4c08
kannan-ravi <kannanravindran00kr@gmail.com>
2023-11-19 14:35:41 +0530

    Implemented the redux persist to persist user info

--- client/src/app/store.jsx
+++ client/src/app/store.jsx
@@ -1,6 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "./features/UserSlice";
 
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
